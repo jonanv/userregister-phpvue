e884940aaab35a1efcb3acc1af73429fbf258024
Johanny Vargas González <jonan-vargas23@hotmail.com>
2021-10-06 21:55:01 -0500

    Se adelanta la vista y contruccion del login

--- login/index.php
+++ login/index.php
@@ -1,3 +1,29 @@
 <?php require_once "../includes/header.php"; ?>
-<h1>Contenido</h1>
-<?php require_once "../includes/footer.php"; ?>
+
+<div class="container">
+
+    <div class="row">
+        <div class="col s12 m12 l12">
+            <div class="card">
+                <div class="card-content">
+                    <span class="card-title">Card Title</span>
+
+                    <form id="inicioSesion"
+                        autocomplete="off"
+                        @submit.preven="login">
+                        <input type="text"
+                            placeholder="Usuario">
+                    </form>
+
+                </div>
+                <div class="card-action">
+                    <a href="#">This is a link</a>
+                    <a href="#">This is a link</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<?php require_once "../includes/footer.php" ?>
